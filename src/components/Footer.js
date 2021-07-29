5a4c88b70eae252b957cb56802cce2c1947ae0f6
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 20:57:33 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,6 +25,7 @@ export default class Footer extends React.Component {
                     ))}
                   </div>
                   )}
+                  
                 </div>
               </div>
             </footer>
